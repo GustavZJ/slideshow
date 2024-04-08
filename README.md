a73df8c40b985ba07cec57af7077da74781f3123
Gustavio <gust3371@gmail.com>
2024-04-08 11:41:33 +0200

    Finish install script

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 CfA Billedramme Page
 
 To install run install.sh as root
+
+Requirements:
+1: git
--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
+
+cd /var/www/slideshow
+bash update.sh
+
 cp /var/www/slideshow/installFiles/rc.local /etc/rc.local
