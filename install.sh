48d179716742149ab355ec7ada53d421457e954b
Gustavio <gust3371@gmail.com>
2024-04-11 11:20:04 +0200

    Attempt to add file upload

--- install.sh
+++ install.sh
@@ -4,8 +4,5 @@ cd /var/www/slideshow
 bash update.sh
 
 apt-get update
-apt-get install git -y
-apt-get install apache2 -y
-apt-get install feh -y
-apt-get install libheif1 -y
+apt-get install git apache2 php libapache2-mod-php feh libheif1 -y
 cp /var/www/slideshow/installFiles/rc.local /etc/rc.local
---
+++ upload.php
@@ -0,0 +1,21 @@
+
+<html>
+<body>
+<?php
+
+/* Get the name of the file uploaded to Apache */
+$filename = $_FILES['file']['name'];
+
+/* Prepare to save the file upload to the upload folder */
+$location = "/home/panda/Pictures/".$filename;
+
+/* Permanently save the file upload to the upload folder */
+if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) { 
+  echo '<p>The HTML5 and php file upload was a success!</p>'; 
+} else { 
+  echo '<p>The php and HTML5 file upload failed.</p>'; 
+}
+
+?>
+</body>
+</html>
