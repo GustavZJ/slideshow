d83a4747de134b8d15696fb4b818ff1b0cbd4a37
Gustavio <gust3371@gmail.com>
2024-04-11 14:20:54 +0200

    Add more debug data

--- upload.php
+++ upload.php
@@ -16,6 +16,7 @@ if ( move_uploaded_file($_FILES['file']['tmp_name'], $location) ) {
   echo '<p>The php and HTML5 file upload failed.</p>';
   echo '<p>Location:'.$location.'</p>';
   echo '<p>Filename:'.$filename.'</p>';
+  echo $_FILES['file']['tmp_name'];
 }
 
 ?>
