d6182333fad8a1817100eb30fdad25238ddf47f0
Gustavio <gust3371@gmail.com>
2024-04-12 18:50:29 +0200

    Attempt file move fix

--- upload.php
+++ upload.php
@@ -43,7 +43,7 @@ if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-  if (move_uploaded_file($_FILES["file"]["name"], $target_dir)) {
+  if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
     echo "The file " . htmlspecialchars(basename($_FILES["file"]["name"])) . " has been uploaded.";
   } else {
     echo "Sorry, there was an error uploading your file.";
