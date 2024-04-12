be014feab0fbe82d8d2e06ddc96d108de303997f
Gustavio <gust3371@gmail.com>
2024-04-12 18:37:10 +0200

    Fix _FILES debug

--- upload.php
+++ upload.php
@@ -50,7 +50,7 @@ if ($uploadOk == 0) {
   }
 }
 
-echo 'File info: '.$_FILES;
+echo '<br>File info: '.print_r($_FILES);
 echo '<br>BaseName:'.$_FILES["name"]["name"];
 echo '<br>BaseName 2:'.$_FILES["name"];
 echo '<br>File:'. $target_file.'<br>';
