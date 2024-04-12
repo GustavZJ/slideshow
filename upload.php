4044e7539209a51a0e161fe8d2d91857bade2471
Gustavio <gust3371@gmail.com>
2024-04-12 18:21:29 +0200

    Fix php for sure

--- upload.php
+++ upload.php
@@ -3,13 +3,13 @@
 
 <?php
 $target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["name"]);
+$target_file = $target_dir . basename($_FILES["name"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
 // Check if image file is a actual image or fake image
 if (isset($_POST["submit"])) {
-  $check = getimagesize($_FILES["tmp_name"]);
+  $check = getimagesize($_FILES["tmp_name"]["tmp_name"]);
   if ($check !== false) {
     echo "File is an image - " . $check["mime"] . ".";
     $uploadOk = 1;
@@ -43,7 +43,7 @@ if ($uploadOk == 0) {
   echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
 } else {
-  if (move_uploaded_file($_FILES["tmp_name"], $target_file)) {
+  if (move_uploaded_file($_FILES["tmp_name"]["tmp_name"], $target_file)) {
     echo "The file " . htmlspecialchars(basename($_FILES["name"]["name"])) . " has been uploaded.";
   } else {
     echo "Sorry, there was an error uploading your file.";
