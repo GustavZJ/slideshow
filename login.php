be7732622ad03f0b3bce86ecdde0579990d83c4c
Gustavio <gust3371@gmail.com>
2024-04-14 15:54:05 +0200

    Add debug to login

--- login.php
+++ login.php
@@ -6,10 +6,12 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     // Retrieve user data from the database based on username or email
     // Compare the hashed password from the database with the provided password
     if ($validCredentials) {
+        echo 'success';
         $_SESSION["user_id"] = $userId; // Store user ID in the session
         header("Location: restricted/"); // Redirect to the dashboard
         exit();
     } else {
+        echo 'failure';
         $error = "Invalid credentials. Please try again.";
     }
 }
