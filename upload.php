b5e73c755471f4fcda6caad1bb492236c3bc6ea2
Gustavio <gust3371@gmail.com>
2024-04-18 11:04:03 +0200

    Move file upload to the correct form

--- upload.php
+++ upload.php
@@ -1,36 +1,60 @@
-<html>
-<body>
-
 <?php
-$image = new Imagick($_POST['url']);
+$target_dir = "uploads/";
+echo "<script>console.log('" . json_encode($_FILES) . "');</script>";
+foreach(range(0, count($_FILES['files']['name']) - 1) as $x) {
+  $target_file = $target_dir . basename($_FILES['files']["name"][$x]);
+  $uploadOk = 1;
+$imageFileType = strtolower($_FILES['files']['type'][$x]);
 
-function getimg($url)
-{
-  // $headers[] = 'Accept: image/gif, image/x-bitmap, image/jpeg, image/pjpeg, image/jpg, image/jpg';
-  $headers[] = 'Connection: Keep-Alive';
-  $headers[] = 'Content-type: application/x-www-form-urlencoded;charset=UTF-8';
-  $user_agent = 'php';
-  $process = ($url);
-  curl_setopt($process, CURLOPT_HTTPHEADER, $headers);
-  curl_setopt($process, CURLOPT_HEADER, 0);
-  curl_setopt($process, CURLOPT_USERAGENT, $user_agent); //check here         
-  curl_setopt($process, CURLOPT_TIMEOUT, 30);
-  curl_setopt($process, CURLOPT_RETURNTRANSFER, 1);
-  curl_setopt($process, CURLOPT_FOLLOWLOCATION, 1);
-  $return = curl_exec($process);
-  curl_close($process);
-  return $return;
+// Check if image file is an actual image or fake image
+if (isset($_POST["submit"])) {
+  $check = getimagesize($_FILES['files']["tmp_name"][$x]);
+  if ($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
 }
 
-$imagename = basename($img_url);
-if (file_exists('uploads/' . $imagename)) {
-  echo 'File already exists';
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
 }
 
-$image = getimg($img_url);
-file_put_contents('uploads/' . $imagename, $image);
+// Check file size
+if ($_FILES['files']["size"][$x] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if (!str_contains($imageFileType, 'image')) {
+  echo "Sorry, " . $imageFileType . ' is not allowed';
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES['files']["tmp_name"][$x], $target_file)) {
+    echo "The file " . htmlspecialchars(basename($_FILES['files']["name"][$x])) . " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+
+$dirFiles = scandir($target_dir);
+foreach ($dirFiles as $dirFile) {
+  $filePath = $dirPath . '/' . $file;
+  if (is_file($filePath)) {
+      echo $file . "<br>";
+  }
+}    
+}
+
 ?>
-
-
-</body>
-</html>
