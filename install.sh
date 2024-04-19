ef4f18d28b95c626cda7325954e0894454770d1b
Gustavio <gust3371@gmail.com>
2024-04-19 11:30:30 +0200

    ..

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ cp /var/www/slideshow/installFiles/rc.local /etc/rc.local
 cp /var/www/slideshow/installFiles/php.ini /etc/php/8.2/apache2/php.ini
 cp /var/www/slideshow/installFiles/slideshow.conf /etc/apache2/sites-available/
 a2dissite 000-default.conf
+a2dissite slideshow.conf
 a2ensite slideshow.conf
 bash update.sh
 
