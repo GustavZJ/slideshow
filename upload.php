03acf8e02ee2ef94c6a79bd206b3da1aa0b4edc2
Gustavio <gust3371@gmail.com>
2024-04-19 14:13:55 +0200

    Stuff

--- upload.php
+++ upload.php
@@ -1,37 +1,29 @@
 <?php
   $target_dir = "uploads/";
   foreach(range(0, count($_FILES['files']['name']) - 1) as $x) {
-  $target_file = $target_dir . basename($_FILES['files']["name"][$x]);
-  $imageFileType = strtolower($_FILES['files']['type'][$x]);
-
-  // Check if image file is an actual image or fake image
-  if (isset($_POST["submit"])) {
-    $check = getimagesize($_FILES['files']["tmp_name"][$x]);
-    if(!@is_array(getimagesize($_FILES['files']["tmp_name"][$x]))){
-      header("Location: index.html?response=notImage");
+    $target_file = $target_dir . basename($_FILES['files']["name"][$x]);
+    $imageFileType = strtolower($_FILES['files']['type'][$x]);
+    header("Location: index.html?response=$imageFileType");
+    // Check if image file is an actual image or fake image
+    if (str_contains($imageFileType, 'image') == FALSE) {
+      header("Location: index.html?response=notAllowed");
     }
-  }
-
-  // Check if file already exists
-  if (file_exists($target_file) == TRUE) {
-    header("Location: index.html?response=fileExists");
-  }
-
-  // Check file size
-  if ($_FILES['files']["size"][$x] > 500000) {
-    header("Location: index.html?response=tooLarge");
-  }
-
-  // Allow certain file formats
-  if (str_contains($imageFileType, 'image') == FALSE) {
-    header("Location: index.html?response=notAllowed");
-  }
-
-  // Check if $uploadOk is set to 0 by an error
-  if (move_uploaded_file($_FILES['files']["tmp_name"][$x], $target_file)) {
-    header("Location: index.html?response=success");
-  } else {
-    header("Location: index.html?response=error");
-  }
+  
+    // Check if file already exists
+    if (file_exists($target_file) == TRUE) {
+      header("Location: index.html?response=fileExists");
+    }
+  
+    // Check file size
+    if ($_FILES['files']["size"][$x] > 500000) {
+      header("Location: index.html?response=tooLarge");
+    }
+  
+    // Check if $uploadOk is set to 0 by an error
+    if (move_uploaded_file($_FILES['files']["tmp_name"][$x], $target_file)) {
+      header("Location: index.html?response=success");
+    } else {
+      header("Location: index.html?response=error");
+    }
   }
 ?>
