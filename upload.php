b79022df07d2b31d4e7cb95fce0b0d172f0f5755
Gustavio <gust3371@gmail.com>
2024-04-19 13:53:28 +0200

    Remove echo messages

--- upload.php
+++ upload.php
@@ -8,38 +8,30 @@ $imageFileType = strtolower($_FILES['files']['type'][$x]);
 // Check if image file is an actual image or fake image
 if (isset($_POST["submit"])) {
   $check = getimagesize($_FILES['files']["tmp_name"][$x]);
-  if ($check !== false) {
-    echo "File is an image - " . $check["mime"] . ".";
-    $uploadOk = 1;
-  } else {
-    echo "File is not an image.";
+  if ($check == false) {
     header("Location: index.html?response=notImage");
   }
 }
 
 // Check if file already exists
 if (file_exists($target_file)) {
-  echo "Sorry, file already exists.";
+  header("Location: index.html?response=fileExists");
 }
 
 // Check file size
 if ($_FILES['files']["size"][$x] > 500000) {
-  echo "Sorry, your file is too large.";
   header("Location: index.html?response=tooLarge");
 }
 
 // Allow certain file formats
 if (!str_contains($imageFileType, 'image')) {
-  echo "Sorry, " . $imageFileType . ' is not allowed';
   header("Location: index.html?response=notAllowed");
 }
 
 // Check if $uploadOk is set to 0 by an error
 if (move_uploaded_file($_FILES['files']["tmp_name"][$x], $target_file)) {
-  echo "The file " . htmlspecialchars(basename($_FILES['files']["name"][$x])) . " has been uploaded.";
   header("Location: index.html?response=success");
 } else {
-  echo "Sorry, there was an error uploading your file.";
   header("Location: index.html?response=error");
 }
 }
