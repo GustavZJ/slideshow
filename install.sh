c97b19361c2fc6b4c6bc2965bdbd5b78504e1c01
Gustavio <gust3371@gmail.com>
2024-04-19 13:08:56 +0200

    Forgot to make some paths relative

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ bash update.sh
 
 apt-get update
 apt-get install php8.2 libapache2-mod-php feh libheif1 -y
-cp /var/www/slideshow/installFiles/rc.local /etc/rc.local
-cp /var/www/slideshow/installFiles/php.ini /etc/php/8.2/apache2/php.ini
-cp /var/www/slideshow/installFiles/slideshow.conf /etc/apache2/sites-available/
+cp installFiles/rc.local /etc/rc.local
+cp installFiles/php.ini /etc/php/8.2/apache2/php.ini
+cp installFiles/slideshow.conf /etc/apache2/sites-available/
 a2dissite 000-default.conf
 a2dissite slideshow.conf
 a2ensite slideshow.conf
