6cacaf41b9fd61f724e45ee5c30fa60881ab2e5a
JegErGed <121015320+JegErGed@users.noreply.github.com>
2024-04-20 09:42:54 +0200

    Update install.sh
    Corrected error where the installer would change privileges before creating directories.

--- install.sh
+++ install.sh
@@ -6,12 +6,12 @@ apt-get update
 apt-get install php8.2 libapache2-mod-php feh libheif1 -y
 cp installFiles/rc.local /etc/rc.local
 cp installFiles/php.ini /etc/php/8.2/apache2/php.ini
-cp installFiles/slideshow.conf /etc/apache2/sites-available/
+cp installFiles/slideshow.conf /etc/apache2/sites-available/slideshow.conf
 a2dissite 000-default.conf
 a2dissite slideshow.conf
 a2ensite slideshow.conf
+mkdir uploads
+mkdir backup
 chmod 777 uploads/
 bash update.sh
 
-mkdir uploads
-mkdir backup
