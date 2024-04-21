7c696c5f8b71ae5efd60d6be4d64c5c1abb5d9ff
JegErGed <hoeckjohanged@gmail.com>
2024-04-21 07:50:20 +0200

    testing if combining js files solves module problem

--- src/js/error_message_decoder.js
+++ src/js/error_message_decoder.js
@@ -1,4 +1,4 @@
-import {messageFade} from "/src/js/popup.js";
+
 
 function urlExtractor(url) {
     let startRecording = false;
@@ -80,6 +80,45 @@ function messageDecoder(msg) {
     
     return ["error", errExplanation]
 }
+
+
+
+// Message modal fade in / out
+const messageModalColor = {'error':'white', 'success':'green'};
+let fadeTimer;
+let deleteTimer;
+
+// Modal based timed message
+function messageFade(type, message) {
+    // Create modal
+    const errorModal = document.createElement('div');
+    errorModal.id = 'messageModalContent';
+    errorModal.innerHTML = (`<div> <!-- This div is intentional -->
+    <p id="statusMessage"></p>
+    </div>`)
+
+    // Append modal to HTML
+    document.body.appendChild(errorModal);
+
+    const messageModalContent = document.getElementById('messageModalContent');
+    const statusMessage = document.getElementById('statusMessage');
+
+    // Clear all timers, to prevent new timer from finishing early
+    clearTimeout(fadeTimer);
+    clearTimeout(deleteTimer);
+    messageModalContent.style.backgroundColor = messageModalColor[type.toLowerCase()]; // Set matching color of modal
+    statusMessage.innerHTML = message; // Set message
+    setTimeout(() => {
+        messageModalContent.style.opacity = 1; // This somehow ensure that it fades in, even when appended from JS
+    }, 0);
+    fadeTimer = setTimeout(() => {
+        messageModalContent.style.opacity = 0; // Modal fade out
+        deleteTimer = setTimeout(() => {
+            errorModal.remove(); // Remove modal after fade out
+        }, 250)
+    }, 5000);
+}
+
 
 
 
