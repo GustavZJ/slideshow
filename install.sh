2ee779c578dcbb2c2db6f4513244f5e55e2d3e7d
Gustavio <gust3371@gmail.com>
2024-04-29 11:10:46 +0200

    php-dom

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ cd /var/www/slideshow/
 bash update.sh
 
 apt-get update
-apt-get install php libapache2-mod-php feh libheif1 -y
+apt-get install php libapache2-mod-php php-dom feh libheif1 -y
 cp installFiles/rc.local /etc/rc.local
 cp installFiles/slideshow.conf /etc/apache2/sites-available/slideshow.conf
 a2dissite 000-default.conf
