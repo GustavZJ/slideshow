d1fa60b52ae3cbb63df67022f0029f08447ec3c5
Gustavio <gust3371@gmail.com>
2024-04-29 10:19:56 +0200

    DEbug fix

--- admin/imageManager.php
+++ admin/imageManager.php
@@ -14,7 +14,7 @@
         <?php 
             $images = scandir('../uploads');
             foreach($images as $image) {
-                echo $image;
+                echo $image.'<br>';
                 if (!$image == '.' || !$image == '..') {
                     echo '<img style="max-height: 15svh;" src="../uploads/'.$image.'">';
                 }
