baac9fe7d88adcb0f17002937389c3eb4884bc6e
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-02 15:30:35 +0200

    added testdata function

--- addtest.sh
+++ addtest.sh
@@ -1,2 +1,2 @@
-cd "/var/www/slideshow/uploads"
-git archive "https://github.com/ckenst/images-catalog/tree/1abf0f2cb5780abe8dc82ae69be3afe3a045e3ad"
+cd var/www/slideshow
+cp -a ../testdata/ ../uploads/
