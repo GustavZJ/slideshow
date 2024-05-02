cb3591a94164414a76f7ec791d06750311497f7e
Gustavio <gust3371@gmail.com>
2024-05-02 11:11:38 +0200

    test 2

--- admin/imageManager.php
+++ admin/imageManager.php
@@ -33,7 +33,7 @@
             $elements = $dom->getElementById('uploadedImagesCont');
             
             foreach($elements->childNodes as $child) {
-                echo $child->textContent;
+                echo $child->nodeType;
             }
 
         //     if(array_key_exists('deleteBtn', $_POST)) { 
