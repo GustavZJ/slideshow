88ed812d9c420163aab7d5e2b2c2796488ca07f4
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-02 10:51:43 +0200

    ..

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ if ! test -f etc/apache2/.htpasswd; then
     echo Enter the password for the upload user. This will be needed when uploading pictures. \n
     read uploadpasswd
     echo Enter the password for the admin user. This will be needed when changing settings and removing pictures. \n
-    read adminpasswd
+    read adminpasswd;
     
     htpasswd -b -c /etc/apache2/.htpasswd uploader $uploaderpasswd
     htpasswd -b /etc/apache2/.htpasswd admin $adminpasswd
