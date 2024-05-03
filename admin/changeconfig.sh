aa9ec49de8ecd644281729dc2cc4cd9c26144ef7
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-03 09:55:28 +0200

    added debug

--- admin/changeconfig.sh
+++ admin/changeconfig.sh
@@ -4,6 +4,7 @@ upload_max_filesize=$1
 post_max_size=$2
 time_delay=$s3
 
+echo upload_max_filesize post_max_size time_delay
 for key in upload_max_filesize post_max_size max_execution_time max_input_time
 do
  sed -i "s/^\($key\).*/\1 $(eval echo = \${$key})/" /var/www/slideshow/php.ini
