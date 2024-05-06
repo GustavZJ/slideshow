c181336b6ed5045da70a5ff827c60dde41022ff2
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-06 11:35:15 +0200

    add debug

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -7,7 +7,7 @@ $upload_max_filesize = $_POST["upload_max_filesize"];
 $maxamount = $_POST["maxamount"];
 
 $post_max_size = strval(intval($upload_max_filesize)*intval($maxamount))."M";
-
+echo get_current_user();
  $dump = shell_exec('./changeconfig.sh '. $upload_max_filesize."M ". $post_max_size." ". $timedelay);
 // header('location: config.html');
 // exit();
