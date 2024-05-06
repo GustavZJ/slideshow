e48e83b3700fa9084071b629d7b94573971826ef
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-06 10:25:14 +0200

    kjdjkwd

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -1,4 +1,11 @@
 <?php
 echo("<script>console.log('PHP: " . json_encode($_POST) . "');</script>");
+
+$timedelay = $_POST["time_delay"];
+$maxfilesize = $_POST["maxfilesize"];
+$maxamount = $_POST["maxamount"];
+
+echo $timedelay $maxfilesize $maxamount
+
 // header('location: config.html');
 // exit();
