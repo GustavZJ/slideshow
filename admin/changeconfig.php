88e7cc068fdbed172c39a3233300c7a11627713f
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-06 11:16:32 +0200

    it works

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -8,7 +8,6 @@ $maxamount = $_POST["maxamount"];
 
 $post_max_size = strval(intval($upload_max_filesize)*intval($maxamount))."M";
 
-system('echo "PASS" | sudo -u root -S COMMAND');
- echo "shell_exec("'./changeconfig.sh '. $upload_max_filesize."M ". $post_max_size." ". $timedelay")";
+ echo("shell_exec("'./changeconfig.sh '. $upload_max_filesize."M ". $post_max_size." ". $timedelay")");
 // header('location: config.html');
 // exit();
