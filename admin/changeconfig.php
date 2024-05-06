e3e03bf640597742c5d003fffba079b0d5c0d0f5
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-06 11:47:30 +0200

    added failsafe

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -2,6 +2,8 @@
 echo("<script>console.log('PHP: " . json_encode($_POST) . "');</script>");
 
 
+if(!$_POST['timedelay'] or !$_POST['upload_max_filesize'] or !$_POST['maxamount']) 
+
 $timedelay = $_POST['timedelay'];
 $upload_max_filesize = $_POST["upload_max_filesize"];
 $maxamount = $_POST["maxamount"];
@@ -9,5 +11,5 @@ $maxamount = $_POST["maxamount"];
 $post_max_size = strval(intval($upload_max_filesize)*intval($maxamount))."M";
 echo get_current_user();
  $dump = shell_exec('./changeconfig.sh '. $upload_max_filesize."M ". $post_max_size." ". $timedelay);
-// header('location: config.html');
-// exit();
+header('location: config.html');
+exit();
