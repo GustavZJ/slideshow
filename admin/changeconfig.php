643cd239534f97806de93a985314859179a11a3a
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-06 10:35:44 +0200

    bbnb

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -1,6 +1,8 @@
 <?php
 echo("<script>console.log('PHP: " . json_encode($_POST) . "');</script>");
 
+
+echo ($_POST['time_delay']);
 // $timedelay = $_POST['time_delay'];
 // $maxfilesize = $_POST["maxfilesize"];
 // $maxamount = $_POST["maxamount"];
