13a7ed8eb9f7eea5764d4a9e4f1567067f1ba194
Gustavio <gust3371@gmail.com>
2024-05-13 11:13:07 +0200

    Move error message modal to seperate file

---
+++ src/js/errorMessage.js
@@ -0,0 +1,36 @@
+export {messageFade}
+
+// Modal based timed message
+function messageFade(type, message) {
+    if(type == "no_msg") {
+        return null
+    }
+    
+    // Create modal
+    const errorModal = document.createElement('div');
+    errorModal.id = 'messageModalContent';
+    errorModal.innerHTML = (`<div> <!-- This div is intentional -->
+    <p id="statusMessage"></p>
+    </div>`)
+
+    // Append modal to HTML
+    document.body.appendChild(errorModal);
+
+    const messageModalContent = document.getElementById('messageModalContent');
+    const statusMessage = document.getElementById('statusMessage');
+
+    // Clear all timers, to prevent new timer from finishing early
+    clearTimeout(fadeTimer);
+    clearTimeout(deleteTimer);
+    messageModalContent.style.backgroundColor = messageModalColor[type.toLowerCase()]; // Set matching color of modal
+    statusMessage.innerHTML = message; // Set message
+    setTimeout(() => {
+        messageModalContent.style.opacity = 1; // This somehow ensure that it fades in, even when appended from JS
+    }, 0);
+    fadeTimer = setTimeout(() => {
+        messageModalContent.style.opacity = 0; // Modal fade out
+        deleteTimer = setTimeout(() => {
+            errorModal.remove(); // Remove modal after fade out
+        }, 250)
+    }, 5000);
+}
--- src/js/error_message_decoder.js
+++ src/js/error_message_decoder.js
@@ -1,3 +1,5 @@
+import {messageFade} from '/src/js/errorMessage.js'
+
 function urlExtractor(url) {
     let recording = false;
     let msg = "";
@@ -90,44 +92,6 @@ function messageDecoder(msg) {
 const messageModalColor = {'error':'white', 'success':'green'};
 let fadeTimer;
 let deleteTimer;
-
-// Modal based timed message
-function messageFade(type, message) {
-    if(type == "no_msg") {
-        return null
-    }
-    
-    // Create modal
-    const errorModal = document.createElement('div');
-    errorModal.id = 'messageModalContent';
-    errorModal.innerHTML = (`<div> <!-- This div is intentional -->
-    <p id="statusMessage"></p>
-    </div>`)
-
-    // Append modal to HTML
-    document.body.appendChild(errorModal);
-
-    const messageModalContent = document.getElementById('messageModalContent');
-    const statusMessage = document.getElementById('statusMessage');
-
-    // Clear all timers, to prevent new timer from finishing early
-    clearTimeout(fadeTimer);
-    clearTimeout(deleteTimer);
-    messageModalContent.style.backgroundColor = messageModalColor[type.toLowerCase()]; // Set matching color of modal
-    statusMessage.innerHTML = message; // Set message
-    setTimeout(() => {
-        messageModalContent.style.opacity = 1; // This somehow ensure that it fades in, even when appended from JS
-    }, 0);
-    fadeTimer = setTimeout(() => {
-        messageModalContent.style.opacity = 0; // Modal fade out
-        deleteTimer = setTimeout(() => {
-            errorModal.remove(); // Remove modal after fade out
-        }, 250)
-    }, 5000);
-}
-
-
-
 
 //const url = "http://192.168.1.15/index.html?response=Forest-Pixel-Lands-02-SLD-672_totallynot_the_same.jpg%D1%8DfileExists%D1%8D"
 //console.log(messageDecoder(urlExtractor(testUrl)));
