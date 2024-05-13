460948569e7bb7d1973cf439cc649d661941f68d
Gustavio <gust3371@gmail.com>
2024-05-13 10:13:03 +0200

    There we go

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -3,15 +3,15 @@ echo("<script>console.log('PHP: " . json_encode($_POST) . "');</script>");
 
 
 $timedelay = $_POST['timedelay'];
-$uploadmaxfilesize = $_POST["uploadmaxfilesize"];
+$maxfilesize   = $_POST["maxfilesize"];
 $maxamount = $_POST["maxamount"];
 
 
 
-$post_max_size = strval(intval($uploadmaxfilesize)*intval($maxamount))."M";
+$post_max_size = strval(intval($maxfilesize)*intval($maxamount))."M";
 echo get_current_user();
 shell_exec("../clear.sh");
-// $dump = shell_exec("./changeconfig.sh ". $uploadmaxfilesize."M ". $post_max_size." ". $timedelay);
+// $dump = shell_exec("./changeconfig.sh ". $maxfilesize."M ". $post_max_size." ". $timedelay);
 // echo $dump;
 // header('location: config.html');
 // exit();
