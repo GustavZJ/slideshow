3d55b9a816a6df236d1dc6382d8489de2e772dca
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-13 08:58:25 +0200

    ups

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -10,4 +10,4 @@ $post_max_size = strval(intval($upload_max_filesize)*intval($maxamount))."M";
 echo get_current_user();
  $dump = shell_exec('./changeconfig.sh '. $upload_max_filesize."M ". $post_max_size." ". $timedelay);
 // header('location: config.html');
-exit();
+// exit();
