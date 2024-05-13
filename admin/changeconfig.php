7b6300ac418725f51926adaf89b81585ce62baa1
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-13 08:57:57 +0200

    fixed it for sure

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -2,10 +2,6 @@
 echo("<script>console.log('PHP: " . json_encode($_POST) . "');</script>");
 
 
-// if(!$_POST['timedelay'] or !$_POST['upload_max_filesize'] or !$_POST['maxamount']) {
-//     exit();
-// }
-
 $timedelay = $_POST['timedelay'];
 $upload_max_filesize = $_POST["upload_max_filesize"];
 $maxamount = $_POST["maxamount"];
@@ -13,5 +9,5 @@ $maxamount = $_POST["maxamount"];
 $post_max_size = strval(intval($upload_max_filesize)*intval($maxamount))."M";
 echo get_current_user();
  $dump = shell_exec('./changeconfig.sh '. $upload_max_filesize."M ". $post_max_size." ". $timedelay);
-header('location: config.html');
+// header('location: config.html');
 exit();
