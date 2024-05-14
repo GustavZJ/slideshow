0d0b198b1db1a5c0fab440b51a09173ed01606fc
Gustavio <gust3371@gmail.com>
2024-05-14 18:01:18 +0200

    Fix 2?

--- admin/imageManager.php
+++ admin/imageManager.php
@@ -82,7 +82,7 @@
                                     errMsg += `${deleteCount} billede(r) blev fjernet`;
                                 }
                                 errMsg += (`Fejl, disse billeder blev ikke fjernet:
-                                ${[...errorList].join('\n')}`);
+                                ${[...errorList].join('<br>')}`);
                                 messageFade('error', errMsg);
                             }
                             deleteCount = 0;
