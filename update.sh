9a5584ab5163e947f4cb44810b05519a2e9c02bf
Gustavio <gust3371@gmail.com>
2024-05-15 19:34:57 +0200

    Maybe

--- update.sh
+++ update.sh
@@ -4,14 +4,14 @@ git reset --hard
 
 git pull https://github.com/GustavZJ/slideshow.git
 
-readme=$(< README.md)
 message=$(git log -1)
 
 curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
   --mail-from 'updatereminder18@gmail.com' \
   --mail-rcpt 'gust3371@gmail.com' \
   --user 'updatereminder18@gmail.com:jjod zvvi lyoh lnbq' \
-  -T <(echo -e 'From: updatereminder18@gmail.com\nTo: gust3371@gmail.com\nSubject: Super Crytikal Iformazion!!\n\n The newest commit message is '{$message}'<br><!DOCTYPE html><body>'{$readme}'</body></html>')
+  --upload-file 'README.md' \
+  -T <(echo -e 'From: updatereminder18@gmail.com\nTo: gust3371@gmail.com\nSubject: Super Crytikal Iformazion!!\n\n The newest commit message is '{$message}'')
 
 chmod 744 admin/changeconfig.sh
 chown www-data:www-data admin/changeconfig.sh
