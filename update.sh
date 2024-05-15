aabdc622c3daa01f3df9fcf9d63d5556ad7fb187
Gustavio <gust3371@gmail.com>
2024-05-15 19:36:59 +0200

    Fix

--- update.sh
+++ update.sh
@@ -10,8 +10,8 @@ curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
   --mail-from 'updatereminder18@gmail.com' \
   --mail-rcpt 'gust3371@gmail.com' \
   --user 'updatereminder18@gmail.com:jjod zvvi lyoh lnbq' \
-  --upload-file 'README.md' \
-  -T <(echo -e 'From: updatereminder18@gmail.com\nTo: gust3371@gmail.com\nSubject: Super Crytikal Iformazion!!\n\n The newest commit message is '{$message}'')
+  -T <(echo -e 'From: updatereminder18@gmail.com\nTo: gust3371@gmail.com\nSubject: Super Crytikal Iformazion!!\n\n The newest commit message is '{$message}'') \
+  --upload-file 'README.md'
 
 chmod 744 admin/changeconfig.sh
 chown www-data:www-data admin/changeconfig.sh
