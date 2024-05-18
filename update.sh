6d362f706c061016b962ecf2f6c563d629d6bd75
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-18 12:25:12 +0200

    only send message if there is a new update.

--- update.sh
+++ update.sh
@@ -1,17 +1,21 @@
 cd /var/www/slideshow
 
+old_message=$(git log -1)
+
 git reset --hard
 
 git pull https://github.com/GustavZJ/slideshow.git
 
 #message=$(git log -1)
 
-#curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
-#  --mail-from 'updatereminder18@gmail.com' \
-#  --mail-rcpt 'gust3371@gmail.com' \
-#  --user 'updatereminder18@gmail.com:jjod zvvi lyoh lnbq' \
-#  -T <(echo -e 'From: updatereminder18@gmail.com\nTo: gust3371@gmail.com\nSubject: Super Crytikal Iformazion!!\n\n The newest commit message is '{$message}'') \
-#  --upload-file 'README.md'
+if [$old_message!=$message]
+    curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
+ --mail-from 'updatereminder18@gmail.com' \
+ --mail-rcpt 'gust3371@gmail.com' \
+ --user 'updatereminder18@gmail.com:jjod zvvi lyoh lnbq' \
+ -T <(echo -e 'From: updatereminder18@gmail.com\nTo: gust3371@gmail.com\nSubject: Super Crytikal Iformazion!!\n\n The newest commit message is '{$message}'') \
+ --upload-file 'README.md'
+fi
 
 chmod 744 admin/changeconfig.sh
 chown www-data:www-data admin/changeconfig.sh
