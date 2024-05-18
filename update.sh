4619e28f1738e14b0388519ef3e3f5291da0137e
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-18 16:10:13 +0200

    added email

--- update.sh
+++ update.sh
@@ -6,27 +6,33 @@ git reset --hard
 
 git pull https://github.com/GustavZJ/slideshow.git
 
-message=$(git log -1)
+message=$(git log -1 --pretty=%B)
 readme=$(cat README.md)
-touch message.txt
+touch message.html
 
-echo $message >> message.txt
-
-echo $readme >> message.txt
-
-cat message.txt
-isnew=[ $oldmessage != $message ]
+echo "From: "Slideshow Update Reminder" <updatereminder18@gmail.com>" >> message.html
+echo "To: "Henrik Pedersen" <hoeckjohanged@gmail.com>" >> message.html
+echo "Subject: The newest release of slideshow has been installed." >> message.html
+echo "" >> message.html
+echo "<!DOCTYPE html>" >> message.html
+echo "<html>" >> message.html
+echo "<body>" >> message.html
+echo $message >> message.html
+echo $readme >> message.html
+echo "</html>" >> message.html
+echo "</body>" >> message.html
+#isnew=[ $oldmessage != $message ]
 isnew=true
 if [ isnew ]; then
-curl --url 'smtps://smtp.gmail.com:465' --ssl-reqd \
- --mail-from 'updatereminder18@gmail.com' \
- --mail-rcpt 'gust3371@gmail.com' \
- --user 'updatereminder18@gmail.com:jjod zvvi lyoh lnbq' \
- -T <(echo -e 'From: updatereminder18@gmail.com\nTo: gust3371@gmail.com\nSubject: Super Crytikal Iformazion!!\n\n The newest commit message is '{$message}'') \
- --upload-file 'message.txt'
+curl --ssl-reqd \
+  --url 'smtps://smtp.gmail.com:465' \
+  --user 'updatereminder18@gmail.com:jjod zvvi lyoh lnbq' \
+  --mail-from 'updatereminder18@gmail.com' \
+  --mail-rcpt 'hoeckjohanged@gmail.com' \
+  --upload-file message.html
 fi
 
 chmod 744 admin/changeconfig.sh
 chown www-data:www-data admin/changeconfig.sh
 systemctl restart apache2
-#rm message.txt
+rm message.html
