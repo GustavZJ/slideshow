cc5066eec03e0f52da73801330af53b509b0ab28
JegErGed <hoeckjohanged@gmail.com>
2024-05-20 16:52:40 +0200

    added venv activation

--- install.sh
+++ install.sh
@@ -9,7 +9,8 @@ apt-get install php libapache2-mod-php feh libheif1 libheif-examples   -y
 # , python3-email, python3-smtplib, python3-ssl
 
 python3 -m venv .venv
-
+source .venv/bin/activate
+pip install email, ssl, smtplib
 cp installFiles/rc.local /etc/rc.local
 cp installFiles/defaultphp.ini /var/www/slideshow/php.ini
 cp installFiles/defaultconfig.config /var/www/slideshow/config.config
