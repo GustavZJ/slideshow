5a9b112d039b37c7a2df8c250299a4177631dc5f
Gustavio <gust3371@gmail.com>
2024-05-20 17:22:56 +0200

    DEbug

--- src/js/imageManager.js
+++ src/js/imageManager.js
@@ -160,11 +160,12 @@ function dropFile(event) {
             if (file.type.startsWith('image/')) {
                 // Read the file as a Data URI
                 const reader = new FileReader();
+                console.log('1');
                 reader.onload = function(event) {
                     const dataURI = event.target.result;
+                    console.log('2');
                     dataURItoImage(dataURI)
                     .then((image) => {
-                        console.log(image);
                         // You can now use the image object
                         document.body.appendChild(image); // Append the image to the body, for example
                     })
