49cc4d11a4df3427751f4a03ac31f62f13e5cced
JegErGed <hoeckjohanged@gmail.com>
2024-05-20 18:25:43 +0200

    added reciever config

--- activate_email.py
+++ activate_email.py
@@ -2,10 +2,11 @@ print("Hello!\nYou chose to enable email notifications.\nWarning!!!\nThis is an 
 
 email = input("Choose the gmail, that you want to use: ")
 password = input("Input the special key from your account : ")
+receiver = input("Who is the intended recipient?: ")
 
 # This is obviously very safe... 
 with open("/var/www/slideshow/nothing.here", "w") as file:
-    file.write(f"{email}\n{password}")
+    file.write(f"{email}\n{password}\n{receiver}")
 
 
 print("Testing...")
@@ -13,4 +14,5 @@ with open("/var/www/slideshow/nothing.here", "r") as file:
     lines = file.readlines()
     username = lines[0].strip()
     password = lines[1].strip()
-    print(f"USERNAME={username}, PASSWORD={password}")
+    receiver = lines[2].strip()
+    print(f"USERNAME={username}, PASSWORD={password}, RECIPIENT={receiver}")
