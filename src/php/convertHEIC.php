7f1f171e023958871fbddbb58beef2022d923b33
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 16:16:51 +0200

    ..

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -3,6 +3,7 @@
 function convertHeic() {
         $files = scandir('/var/www/slideshow/temp');
     foreach($files as $file) {
+        echo "/var/slideshow/www/temp/".$file;
         if(is_file("/var/slideshow/www/temp/".$file)) {
             echo success;
             Maestroerror\HeicToJpg::convert("/var/slideshow/www/temp/".$file)->saveAs("/var/slideshow/www/temp/".$file.".jpg");
