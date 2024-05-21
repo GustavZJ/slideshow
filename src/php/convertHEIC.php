d9ca08b529298cc5738d622880c6fa108242a936
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 15:59:12 +0200

    this will surely work..

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -2,10 +2,8 @@
 
 function convertHeic() {
         $files = scandir('/var/www/slideshow/temp');
-        echo 'console.log(' . json_encode($files) . ');';
     foreach($files as $file) {
-        echo $file;
-    // Maestroerror\HeicToJpg::convert($file)->saveAs("image1.jpg");
+        Maestroerror\HeicToJpg::convert($file)->saveAs("$file.jpg");
     }
     
 }
