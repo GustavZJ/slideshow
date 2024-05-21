fc0bd727fc4a0be1328fd96892c200271211e655
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 15:16:43 +0200

    added condition of there being a new commit for the email to be send

--- update.sh
+++ update.sh
@@ -1,13 +1,13 @@
 cd /var/www/slideshow
 
-oldmessage=$(git log -1)
+oldmessage=$(git log -1 --pretty=%B)
 
 git reset --hard
 
 git pull https://github.com/GustavZJ/slideshow.git
 
 message=$(git log -1 --pretty=%B)
-subject="Slideshow just updated! The newest commit is $message" 
+subject="Slideshow just updated! The newest commit is \"$message\"" 
 readme=$(cat README.md)
 touch message.html
 
@@ -18,8 +18,8 @@ echo $subject >> message.html
 echo $readme >> message.html
 echo "</html>" >> message.html
 echo "</body>" >> message.html
-if [ -f nothing.here ]; then
-    python3 email_sender.py "\"$subject\"."
+if [ -f nothing.here ] -a [[ "$oldmessage" != "$message" ]]; then
+    python3 email_sender.py "$subject."
 fi
 chmod 744 admin/changeconfig.sh
 chown www-data:www-data admin/changeconfig.sh
