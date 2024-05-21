0e75d1ad787c69ae4c604ef3edf51f361e1d185c
Gustavio <gust3371@gmail.com>
2024-05-21 19:36:02 +0200

    Remove checks

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -49,7 +49,7 @@ return preg_replace_callback('/^\s*(\d+)\s*(?:([kmgt]?)b?)?\s*$/i', function ($m
 // echo "<script>console.log('Debug Objects: " .json_encode($iniFile) . "' );</script>";
 // echo "<script>console.log('Debug Objects: " .json_encode(convertToBytes($iniFile['upload_max_filesize'])) . "' );</script>";
 
-$response = array();
+// $response = array();
 $target_dir = "/var/www/slideshow/temp/";
 foreach(range(0, count($_FILES['hidden']['name']) - 1) as $x) {
     $target_file = $target_dir . basename($_FILES['hidden']["name"][$x]);
@@ -57,23 +57,23 @@ foreach(range(0, count($_FILES['hidden']['name']) - 1) as $x) {
     $uploadOk = 1;
     $response[basename($_FILES['hidden']['name'][$x])] = [];
 
-    // Check if image file is an actual image or fake image
-    if (!str_contains($imageFileType, 'image')) {
-        $uploadOk = 0;
-        array_push($response[basename($_FILES['hidden']['name'][$x])], 'er ikke et billede');
-    }
+    // // Check if image file is an actual image or fake image
+    // if (!str_contains($imageFileType, 'image')) {
+    //     $uploadOk = 0;
+    //     array_push($response[basename($_FILES['hidden']['name'][$x])], 'er ikke et billede');
+    // }
 
-    // Check if file already exists
-    if (file_exists($target_file)) {
-        $uploadOk = 0;
-        array_push($response[basename($_FILES['hidden']['name'][$x])], 'eksisterer allerede');
-    }
+    // // Check if file already exists
+    // if (file_exists($target_file)) {
+    //     $uploadOk = 0;
+    //     array_push($response[basename($_FILES['hidden']['name'][$x])], 'eksisterer allerede');
+    // }
 
-    // Check if file is too large
-    if ($_FILES['hidden']["size"][$x] > convertToBytes($iniFile['upload_max_filesize'])) {
-        $uploadOk = 0;
-        array_push($response[basename($_FILES['hidden']['name'][$x])], 'er for stor');
-    }
+    // // Check if file is too large
+    // if ($_FILES['hidden']["size"][$x] > convertToBytes($iniFile['upload_max_filesize'])) {
+    //     $uploadOk = 0;
+    //     array_push($response[basename($_FILES['hidden']['name'][$x])], 'er for stor');
+    // }
 
     if ($uploadOk){
         if (move_uploaded_file($_FILES['hidden']["tmp_name"][$x], $target_file)) {
