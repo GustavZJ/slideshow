11e78ce5b65789a5f481c5efb41fb795dca61c8b
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 15:51:25 +0200

    convert heic try to loop in dir

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -1,6 +1,11 @@
 <?php
 
 function convertHeic() {
+    $files = scandir('/var/www/slideshow/temp');
+foreach($files as $file) {
+    echo $file;
+    // Maestroerror\HeicToJpg::convert($file)->saveAs("image1.jpg");
+}
     
 }
 
