6666bf18b5d0e1a9c474e5bf3d6f69b45c19ef34
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 16:14:57 +0200

    monke

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -3,9 +3,9 @@
 function convertHeic() {
         $files = scandir('/var/www/slideshow/temp');
     foreach($files as $file) {
-        if(is_file($file)) {
-            echo $file.".jpg";
-            Maestroerror\HeicToJpg::convert($file)->saveAs($file.".jpg");
+        if(is_file("/var/slideshow/www/temp/".$file)) {
+
+            Maestroerror\HeicToJpg::convert("/var/slideshow/www/temp/".$file)->saveAs("/var/slideshow/www/temp/".$file.".jpg");
         }
         
     }
