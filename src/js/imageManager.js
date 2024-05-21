c270217aa72dbf79d937d1591e8b53241a3e5ee0
Gustavio <gust3371@gmail.com>
2024-05-21 18:45:04 +0200

    Now it works?

--- src/js/imageManager.js
+++ src/js/imageManager.js
@@ -135,7 +135,6 @@ async function dropFile(event) {
                             const blob = await response.blob();
                             const file = new File([blob], "testName.jpeg", { type: "image/jpeg" });
                             files.push(file);
-                            addDataUriToFileInput(data, 'testName.jpeg')
                             resolve();
                         } catch (error) {
                             console.error("Error converting DataURI to File:", error);
@@ -150,6 +149,7 @@ async function dropFile(event) {
                                 const blob = await response.blob();
                                 const file = new File([blob], "File name", { type: "image/png" });
                                 files.push(file);
+                                appendFileToInput(file);
                                 resolve();
                             } catch (error) {
                                 console.error("Error converting URL to File:", error);
@@ -214,23 +214,24 @@ async function fetchImageFile(url) {
     return new File([blob], filename, { type: blob.type });
 }
 
-function addDataUriToFileInput(dataUri, fileName) {
-    // Convert data URI to Blob
-    const byteString = atob(dataUri.split(',')[1]);
-    const mimeString = dataUri.split(',')[0].split(':')[1].split(';')[0];
-    const arrayBuffer = new ArrayBuffer(byteString.length);
-    const uint8Array = new Uint8Array(arrayBuffer);
-    for (let i = 0; i < byteString.length; i++) {
-        uint8Array[i] = byteString.charCodeAt(i);
-    }
-    const blob = new Blob([uint8Array], { type: mimeString });
-
-    // Create File object from Blob
-    const file = new File([blob], fileName, { type: mimeString });
-
-    // Set the file input value
+function appendFileToInput(file) {
+    // Get the file input element
     const fileInput = document.getElementById('your-file-input-id');
-    fileInput.files = [file];
+
+    // Create a new array to hold all files (existing + new)
+    const allFiles = Array.from(fileInput.files);
+
+    // Add the new file to the array of files
+    allFiles.push(file);
+
+    // Create a new FileList from the combined array of files
+    const combinedFileList = new DataTransfer();
+    allFiles.forEach(file => {
+        combinedFileList.items.add(file);
+    });
+
+    // Set the files property of the file input to the combined FileList
+    fileInput.files = combinedFileList.files;
 }
 
 
