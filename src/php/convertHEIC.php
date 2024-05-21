346ce1815189fd46552a88ebda952ea7bbb6fb9b
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 15:56:36 +0200

    ssshwshhhhh

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -1,15 +1,17 @@
 <?php
 
 function convertHeic() {
-    $files = scandir('/var/www/slideshow/temp');
-    echo 'console.log(' . json_encode($files) . ');';
-foreach($files as $file) {
-    echo $file;
+        $files = scandir('/var/www/slideshow/temp');
+        echo 'console.log(' . json_encode($files) . ');';
+    foreach($files as $file) {
+        echo $file;
     // Maestroerror\HeicToJpg::convert($file)->saveAs("image1.jpg");
-}
+    }
     
 }
 
+
+convertHeic()
 // header('Content-Type: application/json');
 // echo json_encode($convertedFile);
 // exit();
