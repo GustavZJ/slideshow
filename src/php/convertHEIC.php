c45ff394a13da66c35493308a77c7cf0c1700c3f
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 16:24:20 +0200

    yes?

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -5,10 +5,10 @@ function convertHeic() {
     foreach($files as $file) {
         $file_name = "/var/www/slideshow/temp/".$file;
         echo $file_name."<br>" ;
-        if(is_file($file_name)) {
+        //if(is_file($file_name)) {
             echo success;
             Maestroerror\HeicToJpg::convert("/var/www/slideshow/temp/".$file)->saveAs("/var/www/slideshow/temp/".$file.".jpg");
-        }
+        //}
         
     }
     
