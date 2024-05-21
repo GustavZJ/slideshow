84f4160a1a00fda7190b18fbf8dcd0a7ab9ad5c2
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 16:18:42 +0200

    ssshhhh

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -1,12 +1,12 @@
 <?php
 
 function convertHeic() {
-        $files = scandir('/var/www/slideshow/temp');
+        $files = scandir('/var/www/slideshow/temp/');
     foreach($files as $file) {
-        echo "/var/slideshow/www/temp/".$file;
-        if(is_file("/var/slideshow/www/temp/".$file)) {
+        echo "/var/www/slideshow/temp/".$file;
+        if(is_file("/var/www/slideshow/temp/".$file)) {
             echo success;
-            Maestroerror\HeicToJpg::convert("/var/slideshow/www/temp/".$file)->saveAs("/var/slideshow/www/temp/".$file.".jpg");
+            Maestroerror\HeicToJpg::convert("/var/www/slideshow/temp/".$file)->saveAs("/var/www/slideshow/temp/".$file.".jpg");
         }
         
     }
