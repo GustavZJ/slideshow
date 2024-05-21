5db64254b44a87084da51d74c41c14ab72b43483
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 15:55:02 +0200

    console.log $files var

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -2,7 +2,7 @@
 
 function convertHeic() {
     $files = scandir('/var/www/slideshow/temp');
-    echo $files;
+    echo 'console.log(' . json_encode($files) . ');';
 foreach($files as $file) {
     echo $file;
     // Maestroerror\HeicToJpg::convert($file)->saveAs("image1.jpg");
