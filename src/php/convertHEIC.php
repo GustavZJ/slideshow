4d34bf2ef128b27832c1fb5b1f4db844e0a5b82d
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 18:30:00 +0200

    maybe

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -1,6 +1,6 @@
 <?php
 
-require 'vendor/autoload.php'; // Make sure to include the Composer autoloader
+require '/varwww/slideshow/vendor/autoload.php'; // Make sure to include the Composer autoloader
 
 use Maestroerror\HeicToJpg;
 
