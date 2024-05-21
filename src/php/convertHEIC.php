19ac2071fda0d661dda3b4729f5c28fdae4d175d
JegErGed <hoeckjohanged@gmail.com>
2024-05-21 18:20:55 +0200

    don't say this was the problem

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -1,12 +1,17 @@
 <?php
 
+require 'vendor/autoload.php'; // Make sure to include the Composer autoloader
+
+use Maestroerror\HeicToJpg;
+
+
 function convertHeic() {
         $files = scandir('/var/www/slideshow/temp/');
     foreach($files as $file) {
         $file_name = "/var/www/slideshow/temp/".$file;
         // echo $file_name."<br>" ;
         if(is_file($file_name)) {
-            Maestroerror\php-heic-to-jpg::convert("/var/www/slideshow/temp/".$file, "arm64", true)->saveAs("/var/www/slideshow/temp/".$file.".jpg");
+            Maestroerror\HeicToJpg::convert("/var/www/slideshow/temp/".$file, "arm64", true)->saveAs("/var/www/slideshow/temp/".$file.".jpg");
             echo $file_name." success";
         }
         
