9428a3538a06cfbd6dd8a7016faad98e7b2ece2c
Gustavio <gust3371@gmail.com>
2024-05-22 21:20:33 +0200

    Add cache control to hopefully fix file issue

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -91,5 +91,8 @@ function convertHeic() {
 convertHeic();
 
 header('Content-Type: application/json');
+header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+header('Pragma: no-cache');
+header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
 echo json_encode($outputFiles);
 exit();
