b8412cab99428cb2867a5e78b4d2b52eef3d4d69
JegErGed <hoeckjohanged@gmail.com>
2024-05-22 17:32:13 +0200

    unlinking files

--- src/php/convertHEIC.php
+++ src/php/convertHEIC.php
@@ -1,35 +1,4 @@
 <?php
-$outputFiles = array();
-
-// function convertHeicWithHeifConvert($filePath) {
-//     global $outputFiles;
-//     $outputPath = $filePath . '.jpg';
-//     $command = "heif-convert $filePath $outputPath";
-//     exec($command, $output, $return_var);
-//     if ($return_var === 0) {
-//         echo "$filePath converted successfully to $outputPath\n";
-//         array_push($outputFiles, $outputPath);
-//     } else {
-//         echo "Error converting $filePath\n";
-//     }
-// }
-
-// function convertHeic() {
-//     $directory = '/var/www/slideshow/temp/';
-//     if (!is_dir($directory)) {
-//         echo "Directory does not exist: $directory\n";
-//         return;
-//     }
-
-//     $files = scandir($directory);
-
-//     foreach($files as $file) {
-//         $filePath = $directory . $file;
-//         if (is_file($filePath) && strtolower(pathinfo($filePath, PATHINFO_EXTENSION)) === 'heic') {
-//             convertHeicWithHeifConvert($filePath);
-//         }
-//     }
-// }
 
 // Load php.ini file to read max file size
 $iniFile = parse_ini_file('/var/www/slideshow/php.ini');
@@ -83,7 +52,43 @@ foreach (range(0, count($_FILES['hidden']['name']) - 1) as $x) {
     }
 }
 
-// convertHeic();
+
+$outputFiles = array();
+
+function convertHeicWithHeifConvert($filePath) {
+    global $outputFiles;
+    $outputPath = $filePath . '.jpg';
+    $command = "heif-convert $filePath $outputPath";
+    exec($command, $output, $return_var);
+    if ($return_var === 0) {
+        unlink($filePath);
+        echo "$filePath converted successfully to $outputPath\n";
+        array_push($outputFiles, $outputPath);
+    } else {
+        echo "Error converting $filePath\n";
+    }
+}
+
+function convertHeic() {
+    $directory = '/var/www/slideshow/temp/';
+    if (!is_dir($directory)) {
+        echo "Directory does not exist: $directory\n";
+        return;
+    }
+
+    $files = scandir($directory);
+
+    foreach($files as $file) {
+        $filePath = $directory . $file;
+        if (is_file($filePath) && strtolower(pathinfo($filePath, PATHINFO_EXTENSION)) === 'heic') {
+            convertHeicWithHeifConvert($filePath);
+        }
+    }
+}
+
+
+
+convertHeic();
 
 header('Content-Type: application/json');
 echo json_encode(['errors' => $errorStr, 'convertedFiles' => $outputFiles]);
