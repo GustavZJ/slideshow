5b9f15ef3f147e730695b1d7d00b861c5ffde1ac
Gustavio <gust3371@gmail.com>
2024-05-22 17:42:21 +0200

    Debug

--- src/js/imageManager.js
+++ src/js/imageManager.js
@@ -240,7 +240,7 @@ async function fetchImageFileThroughProxy(url, filename = '') {
         const html = await response.text();
         const imageUrl = extractImageUrlFromHtml(html);
         if (imageUrl) {
-            console.log(imageURL);
+            console.log(imageUrl);
             return fetchImageFileThroughProxy(imageUrl);
         } else {
             throw new Error('Unable to extract image URL from HTML.');
