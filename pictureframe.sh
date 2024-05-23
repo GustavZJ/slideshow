89fabb5af1897a6118820c993efee3c4dd25e93e
JegErGed <hoeckjohanged@gmail.com>
2024-05-23 16:57:51 +0200

    test

--- pictureframe.sh
+++ pictureframe.sh
@@ -21,28 +21,7 @@ cd /var/www/slideshow/uploads
 
 source /var/www/slideshow/config.config
 
-# If you have X or more files, move files that are 6 months or older to /home/pi/Backup/.
 
-	# for f in *;
-	# do
-	# 	file_count=$(ls | wc -l)
-	# 	if [ $file_count -gt 20 ];
-	# 	then
-	# 		str=""
-	# 		for i in {0..7};
-	# 		do
-	# 			str+="${f:i:1}"
-	# 		done
-	# 		cdate="$(date +"%Y%m%d")"
-	# 		cdateint=$((cdate - 600))
-	# 		str=$((str))
-	# 		if [ $str -le $cdateint ];
-	# 		then
-	# 			mv $f ../backup
-
-	# 		fi
-	# 	fi
-	# done
 cd ..
 
-XAUTHORITY="/home/$USER/.Xauthority" DISPLAY=:0.0 feh --auto-rotate -q -p -Z -F -R 60 -Y -D "$timedelay" uploads/
+sudo -u $USER DISPLAY=$DISPLAY XAUTHORITY=$XAUTHORITY feh --auto-rotate -q -p -Z -F -R 60 -Y -D "$timedelay" uploads/
