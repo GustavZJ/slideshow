3b05dc8f3e6fc07907e9f263fb78ecdb4e4cb168
JegErGed <hoeckjohanged@gmail.com>
2024-05-23 17:04:53 +0200

    ..

--- pictureframe.sh
+++ pictureframe.sh
@@ -20,4 +20,4 @@ source /var/www/slideshow/config.config
 cd ..
 
 # Run feh with the specified parameters as the user
-sudo -u $USER env DISPLAY="$DISPLAY" XAUTHORITY="$XAUTHORITY" feh --auto-rotate -q -p -Z -F -R 60 -Y -D "$timedelay" /var/www/slideshow/uploads/
+sudo -u "$USER" env DISPLAY="$DISPLAY" XAUTHORITY="$XAUTHORITY" feh --auto-rotate -q -p -Z -F -R 60 -Y -D "$timedelay" /var/www/slideshow/uploads/
