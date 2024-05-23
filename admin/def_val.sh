759acb635f0862234ef6425ced7d26f059e1e129
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-23 11:12:37 +0200

    commit

--- admin/def_val.sh
+++ admin/def_val.sh
@@ -3,7 +3,7 @@ declare -A values
 for key in upload_max_filesize post_max_size max_execution_time max_input_time max_file_uploads
 do
  value=$(grep "^$key" /var/www/slideshow/php.ini | awk '{print $3}')
- $values[ "$key" ]=$value
+ $values[ "$key" ] = $value
 
 done
 echo "$values"
