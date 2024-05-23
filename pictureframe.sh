b04819874ee0f136a48675f171d7659e7b7af161
Gustavio <gust3371@gmail.com>
2024-05-23 15:01:43 +0200

    Remove unnescissary stuff from pictureframe.sh

--- pictureframe.sh
+++ pictureframe.sh
@@ -5,55 +5,28 @@ cd /var/www/slideshow/uploads
 
 source /var/www/slideshow/config.config
 
+# If you have X or more files, move files that are 6 months or older to /home/pi/Backup/.
 
-#Iterate over all files of file type .HEIC and convert to jpeg. This is because HEIC is a shitty file format that iPhones use. 
-for f in ./*.HEIC;
-do
-	heif-convert "$f" "$f.jpeg";
-	rm $f 
-done
+	# for f in *;
+	# do
+	# 	file_count=$(ls | wc -l)
+	# 	if [ $file_count -gt 20 ];
+	# 	then
+	# 		str=""
+	# 		for i in {0..7};
+	# 		do
+	# 			str+="${f:i:1}"
+	# 		done
+	# 		cdate="$(date +"%Y%m%d")"
+	# 		cdateint=$((cdate - 600))
+	# 		str=$((str))
+	# 		if [ $str -le $cdateint ];
+	# 		then
+	# 			mv $f ../backup
 
-
-
-#Remove all video files.
-for f in ./*.MOV;
-do
-	rm $f 
-done
-
-
-# #Rename all files to to ?+{current date Year Month Day}+{original file name} if this hasn't already been done already.
-# for f in *;
-# do
-# 	first_char="$(printf '%c' "$f")"
-# 	if [ $first_char != "?" ];
-# 	then
-		
-# 		new="?"+"$(date +"%Y%m%d")"+"$f"
-# 		mv "$f" "$new"
-# 	fi
-# done
-# If you have 20 or more files, move files that are 6 months or older to /home/pi/Backup/.
-# for f in *;
-# do
-# 	file_count=$(ls | wc -l)
-# 	if [ $file_count -gt 20 ];
-# 	then
-# 		str=""
-# 		for i in {0..7};
-# 		do
-# 			str+="${f:i:1}"
-# 		done
-# 		cdate="$(date +"%Y%m%d")"
-# 		cdateint=$((cdate - 600))
-# 		str=$((str))
-# 		if [ $str -le $cdateint ];
-# 		then
-# 			mv $f ../backup
-		
-# 		fi
-# 	fi
-# done
+	# 		fi
+	# 	fi
+	# done
 cd ..
 #
 #xhost si:localuser:root
