0de2aa93bbf41e61465cd11240d23d66117f3a84
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-05-23 15:33:53 +0200

    cleaned user input

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -1,13 +1,13 @@
 <?php
-echo("<script>console.log('PHP: " . json_encode($_POST) . "');</script>");
+echo ("<script>console.log('PHP: " . json_encode($_POST) . "');</script>");
 
 
-$timedelay = $_POST['timedelay'];
-$maxsize   = $_POST["maxsize"];
-$maxamount = $_POST["maxamount"];
+$timedelay = preg_replace("/[^0-9.]/", "", $_POST['timedelay']);
+$maxsize = preg_replace("/[^0-9.]/", "", $_POST["maxsize"]);
+$maxamount = preg_replace("/[^0-9.]/", "", $_POST["maxamount"]);
 
-$post_max_size = strval(intval($maxsize)*intval($maxamount))."M";
+$post_max_size = strval(intval($maxsize) * intval($maxamount)) . "M";
 
-shell_exec("./changeconfig.sh ". $maxsize."M ". $post_max_size." ". $timedelay. " ". $maxamount);
+shell_exec("./changeconfig.sh " . $maxsize . "M " . $post_max_size . " " . $timedelay . " " . $maxamount);
 header('location: config.html');
 exit();
