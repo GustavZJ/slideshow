7ee357159aeaf86df527ca8be62f46d86bbc60af
Gustavio <gust3371@gmail.com>
2024-05-23 11:03:51 +0200

    Assign replaced text

--- admin/imageManager.php
+++ admin/imageManager.php
@@ -22,7 +22,7 @@
                     // Load images from rpi, and display them
                     $images = scandir('../uploads');
                     foreach($images as $image) {
-                        str_replace('?', '%3F', $image);
+                        $image = str_replace('?', '%3F', $image);
                         $fullImage = '../uploads/'.$image;
                         if (is_file($fullImage)) {
                             if (str_ends_with(strtolower($fullImage), '.heic') || str_ends_with(strtolower($fullImage), '.heif')) {
