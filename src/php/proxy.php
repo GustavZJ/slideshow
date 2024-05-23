5e0e2d45175ece4cc201ee029eca591993d11d84
Gustavio <gust3371@gmail.com>
2024-05-23 09:42:12 +0200

    Fix

--- src/php/proxy.php
+++ src/php/proxy.php
@@ -1,8 +1,7 @@
 <?php
 if (isset($_GET['url'])) {
     $url = $_GET['url'];
-    $headers = get_headers($url, 1);
-    $result = array();
+    $headers = get_headers($url, 1);;
 
     // Validate URL
     if (filter_var($url, FILTER_VALIDATE_URL)) {
@@ -22,13 +21,10 @@ if (isset($_GET['url'])) {
         curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 
         // Execute the session and store the contents in a variable
-        $result['test'] = curl_exec($ch);
+        $result = curl_exec($ch);
 
         // Get the content type of the response
         $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
-
-        // Get the filename from the URL
-        $result['filename'] = basename(parse_url($url, PHP_URL_PATH));
 
         // Close the cURL session
         curl_close($ch);
