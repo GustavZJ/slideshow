82f3445d4eaf888e05715b2c112729a38663ea5a
JegErGed <hoeckjohanged@gmail.com>
2024-05-25 15:17:55 +0200

    set the right one?

--- admin/changeconfig.php
+++ admin/changeconfig.php
@@ -1,44 +1,40 @@
 <?php
-echo ("<script>console.log('PHP: " . json_encode($_POST) . "');</script>");
 
-
+// Sanitize input
 $timedelay = preg_replace("/[^0-9.]/", "", $_POST['timedelay']);
 $maxsize = preg_replace("/[^0-9.]/", "", $_POST["maxsize"]);
 $maxamount = preg_replace("/[^0-9.]/", "", $_POST["maxamount"]);
 $autoremoveamount = preg_replace("/[^0-9.]/", "", $_POST["removeimagesamount"]);
 $autoremovetime_post = preg_replace("/[^0-9.]/", "", $_POST["removeimagestime"]);
-$autoremovetime_option = preg_replace("/[^0-9.]/", "", $_POST["removeimagestimeperiod"]);; # Add the option from post
+$autoremovetime_option = preg_replace("/[^0-9.]/", "", $_POST["removeimagestimeperiod"]);
 
-$autoremove = false;
-$autoremove_post = preg_replace("/[^0-9.]/", "", $_POST['removeimagestoggle']); // =on not true
-if ($autoremove_post == "on") {
-    $autoremove = "true";
-}
-else {
-    $autoremove = "false";
-}
+// Determine autoremove
+$autoremove = isset($_POST['removeimagestoggle']) && $_POST['removeimagestoggle'] === "on" ? "true" : "false";
 
+// Calculate autoremovetime based on option
 switch ($autoremovetime_option) {
     case "days":
-      $autoremovetime = intval($autoremovetime_post);
-      break;
+        $autoremovetime = intval($autoremovetime_post);
+        break;
     case "months":
-        $autoremovetime = intval($autoremovetime_post)*100;
-      break;
+        $autoremovetime = intval($autoremovetime_post) * 100;
+        break;
     case "years":
-        $autoremovetime = intval($autoremovetime_post)*10000;
-      break;
+        $autoremovetime = intval($autoremovetime_post) * 10000;
+        break;
     default:
-    $autoremovetime = 600;
-  }
+        $autoremovetime = 600;
+}
 
-
-
-
+// Calculate post_max_size
 $post_max_size = strval(intval($maxsize) * intval($maxamount)) . "M";
 
-$command = "./changeconfig.sh " . $maxsize . "M " . $post_max_size . " " . $maxamount . " " . $timedelay. " " . $autoremove. " " . $autoremoveamount. " " . $autoremovetime;
+// Construct the command
+$command = escapeshellcmd("./changeconfig.sh") . " " . escapeshellarg($maxsize . "M") . " " . escapeshellarg($post_max_size) . " " . escapeshellarg($maxamount) . " " . escapeshellarg($timedelay) . " " . escapeshellarg($autoremove) . " " . escapeshellarg($autoremoveamount) . " " . escapeshellarg($autoremovetime);
 
+// Execute the command
 shell_exec($command);
-header('location: config.html');
+
+// Redirect to config.html
+header('Location: config.html');
 exit();
