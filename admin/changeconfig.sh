134c777aa9a3bb126be352fb89bc2bd00342e62b
JegErGed <hoeckjohanged@gmail.com>
2024-05-25 14:02:14 +0200

    added parmas

--- admin/changeconfig.sh
+++ admin/changeconfig.sh
@@ -4,7 +4,9 @@ upload_max_filesize=$1
 post_max_size=$2
 timedelay=$3
 max_file_uploads=$4
-
+autoremove=$5
+autoremoveamount=$6
+autoremovetime=$7
 
 for key in upload_max_filesize post_max_size max_file_uploads
 do
