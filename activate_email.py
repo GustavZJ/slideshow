d763aad570119035289202e0e2cd03c6b37d6c45
JegErGed <hoeckjohanged@gmail.com>
2024-05-27 16:05:05 +0200

    append mode

--- activate_email.py
+++ activate_email.py
@@ -23,5 +23,5 @@ with open("/var/www/slideshow/nothing.here", "r") as file:
     receiver = lines[2].strip()
     print(f"USERNAME={username}, PASSWORD={password}, RECIPIENT={receiver}")
 
-with open("/var/www/slideshow/update.sh", "w") as file:
-    file.write(f"{file.read()} \ngit pull --rebase https://github.com/GustavZJ/slideshow.git email-support")
+with open("/var/www/slideshow/update.sh", "a") as file:
+    file.write("git pull --rebase https://github.com/GustavZJ/slideshow.git email-support")
