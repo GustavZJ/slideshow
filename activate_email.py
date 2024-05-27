e873f8715f88cdd79173f0c7e0a3bc742498a4a1
JegErGed <hoeckjohanged@gmail.com>
2024-05-27 16:03:12 +0200

    write?

--- activate_email.py
+++ activate_email.py
@@ -23,5 +23,5 @@ with open("/var/www/slideshow/nothing.here", "r") as file:
     receiver = lines[2].strip()
     print(f"USERNAME={username}, PASSWORD={password}, RECIPIENT={receiver}")
 
-with open("/var/www/slideshow/update.sh", "rw") as file:
+with open("/var/www/slideshow/update.sh", "w") as file:
     file.write(f"{file.read()} \ngit pull --rebase https://github.com/GustavZJ/slideshow.git email-support")
