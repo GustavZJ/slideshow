9e77cf8e381c0f90ea029523f8375146a4f1ff12
Gustavio <gust3371@gmail.com>
2024-05-31 22:31:48 +0200

    Add some comments to proxy.php

--- src/php/proxy.php
+++ src/php/proxy.php
@@ -1,8 +1,10 @@
 <?php
+// Enable error reporting for debugging
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Check if a 'url' parameter is passed in the query string
 if (isset($_GET['url'])) {
     $url = $_GET['url'];
 
@@ -18,34 +20,42 @@ if (isset($_GET['url'])) {
         exit();
     }
 
+    // Check if the content type is an image
     if (isset($headers['Content-Type']) && strpos($headers['Content-Type'], 'image/') === 0) {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
         $html = curl_exec($ch);
 
+        // Check for cURL errors
         if(curl_errno($ch)) {
             echo 'cURL error: ' . curl_error($ch);
         } else {
+            // Set the appropriate content type and display the image content
             header("Content-Type: " . $headers['Content-Type']);
             echo $html;
         }
 
         curl_close($ch);
     } else {
+        // Initialize a cURL session for non-image content
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
         curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+
         $result = curl_exec($ch);
 
+        // Check for cURL errors
         if(curl_errno($ch)) {
             echo 'cURL error: ' . curl_error($ch);
             curl_close($ch);
             exit();
         }
 
+        // Retrieve the content type of the response
         $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
         if ($contentType === false) {
             echo "Failed to retrieve content type.";
@@ -55,6 +65,7 @@ if (isset($_GET['url'])) {
 
         curl_close($ch);
 
+        // Set the appropriate content type and display the content
         header("Content-Type: $contentType");
         echo $result;
     }
