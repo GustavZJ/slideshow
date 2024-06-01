c555b04d5ae98ca4510915ead5093f32f82ac182
Gustavio <gust3371@gmail.com>
2024-06-01 23:51:37 +0200

    Fixed crontab for real this time

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ mkdir backup
 
 # Create cron job for auto update
 # Define the new cron job
-new_cron_job="@reboot /var/etc/www/slideshow/update.sh &"
+new_cron_job="@reboot /var/www/slideshow/update.sh &"
 
 # Get the current crontab entries
 current_cron=$(crontab -l 2>/dev/null)
