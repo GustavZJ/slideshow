3f914fb00f5648f8bad70d35f22dc4b7b2c4d60c
JegErGed <hoeckjohanged@gmail.com>
2024-06-04 16:21:45 +0200

    yes

--- src/php/redirectToAdmin.php
+++ src/php/redirectToAdmin.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-$docRoot = $_SERVER['DOCUMENT_ROOT'];
+
 
 // Set a session variable to indicate permission
 
 $_SESSION['role'] = 'admin';
 
 // Redirect to the target URL
-header("Location: $docRoot/admin/index.php");
+header("Location: /admin/index.php");
 exit();
