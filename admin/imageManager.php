3fddce6cbb151a58da0279bf757501199bf1efc0
Gustavio <gust3371@gmail.com>
2024-06-04 15:39:38 +0200

    Fix dropdown btn on image Manager page

--- admin/imageManager.php
+++ admin/imageManager.php
@@ -8,6 +8,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="/src/scss/main.css">
 	    <link rel="icon" type="image/x-icon" href="/favicon.ico">
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.js"></script>
         <script type="module" src="/src/js/deleteImage.js"></script>
     </head>
