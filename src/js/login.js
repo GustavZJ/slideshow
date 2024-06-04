db4f32f2d8e7813edc481caa5b47b1116ed1723c
Gustavio <gust3371@gmail.com>
2024-06-04 22:16:07 +0200

    Fix?

--- src/js/login.js
+++ src/js/login.js
@@ -5,6 +5,8 @@ jQuery(document).ready(function ($) {
             type: 'POST',
             url: '/src/php/login.php',
             data: $(this).serialize(),
+            contentType: false,
+            processData: false,
             success: function (response) {
                 console.log(response);
             },
