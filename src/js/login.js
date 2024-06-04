a9dc8afe4e7823bb6d5404d1ddf6f104cb69ef8c
Gustavio <gust3371@gmail.com>
2024-06-04 22:21:50 +0200

    Fiiix?

--- src/js/login.js
+++ src/js/login.js
@@ -1,18 +1,20 @@
-jQuery(window).load(function ($) {
-    $('loginBtn').submit(function (event) {
-        event.preventDefault();
-        $.ajax({
-            type: 'POST',
-            url: '/src/php/login.php',
-            data: $(this).serialize(),
-            contentType: false,
-            processData: false,
-            success: function (response) {
-                console.log(response);
-            },
-            error: function (response) {
-                console.log(response);
-            }
+jQuery(document).ready(function ($) {
+    $(window).load(function () {
+        $('loginBtn').submit(function (event) {
+            event.preventDefault();
+            $.ajax({
+                type: 'POST',
+                url: '/src/php/login.php',
+                data: $(this).serialize(),
+                contentType: false,
+                processData: false,
+                success: function (response) {
+                    console.log(response);
+                },
+                error: function (response) {
+                    console.log(response);
+                }
+            });
         });
-    });
+    })
 })
