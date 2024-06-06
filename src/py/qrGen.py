0d25851275f259440e3301aa5c16cf017c4f75dd
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-06-06 15:04:54 +0200

    fixed typo

--- src/py/qrGen.py
+++ src/py/qrGen.py
@@ -5,8 +5,8 @@ import sys
 
 domain = sys.argv[1]
 url = pyqrcode.create(domain)
-url.png("/var/www/slideshow/src/pictrure/qrcode.png", scale=10)
-url.svg("/var/www/slideshow/src/pictrure/qrcode.svg", scale=10)
+url.png("/var/www/slideshow/src/picture/qrcode.png", scale=10)
+url.svg("/var/www/slideshow/src/picture/qrcode.svg", scale=10)
 
 
 
