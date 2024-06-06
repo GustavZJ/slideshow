7eda1b3b7afa69569f6b4ef83c29fbd3587340aa
Gustavio <gust3371@gmail.com>
2024-06-06 22:17:19 +0200

    Debug

--- src/php/loadImages.php
+++ src/php/loadImages.php
@@ -5,11 +5,11 @@ $docRoot = $_SERVER['DOCUMENT_ROOT'];
 $imagePath = $docRoot . '/uploads';
 $images = scandir($imagePath);
 $files = array();
-array_push($files, $imagePath.' - '.$images);
 
 foreach($images as $image) {
     $fullImage = $imagePath . $image;
     if (is_file($fullImage)) {
+        array_push($files, $image.' - '.$fullImage);
         // URL encode the file path for displaying in the HTML
         $encodedImage = str_replace('?', '%3F', $image);
         $fullImage = $imagePath . $encodedImage;
