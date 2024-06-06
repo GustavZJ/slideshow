7c18cb1aef557ec0bc000e3f6860fd2724e53c3d
Gustavio <gust3371@gmail.com>
2024-06-06 22:18:25 +0200

    Fix?

--- src/php/loadImages.php
+++ src/php/loadImages.php
@@ -7,13 +7,12 @@ $images = scandir($imagePath);
 $files = array();
 
 foreach($images as $image) {
-    $fullImage = $imagePath . $image;
+    $fullImage = $imagePath .'/'. $image;
     if (is_file($fullImage)) {
-        array_push($files, $image.' - '.$fullImage);
         // URL encode the file path for displaying in the HTML
         $encodedImage = str_replace('?', '%3F', $image);
         $fullImage = $imagePath . $encodedImage;
-        // array_push($files, htmlspecialchars($fullImage));
+        array_push($files, htmlspecialchars($fullImage));
     }
 }
 
