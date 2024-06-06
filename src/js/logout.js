62a94d4a2b785bfa9097cb1ad5b0ec19d5bbfe5a
Gustavio <gust3371@gmail.com>
2024-06-06 20:20:41 +0200

    Fix

--- src/js/logout.js
+++ src/js/logout.js
@@ -1,11 +1,13 @@
-jQuery('#topbar').ready(function ($) {
-    $('#logoutBtn').click(function () {
-        $.ajax({
-            type: 'POST',
-            url: '/src/php/logout.php',
-            success: function() {
-                window.location.pathname = '/index.html';
-            }
+jQuery(document).ready(function ($) {
+    $('#topbar').load(function () {
+        $('#logoutBtn').click(function () {
+            $.ajax({
+                type: 'POST',
+                url: '/src/php/logout.php',
+                success: function() {
+                    window.location.pathname = '/index.html';
+                }
+            });
         });
-    });
+    })
 });
