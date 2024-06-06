c274de06680c9257c15df01f24f082fc5efa7ce0
Gustavio <gust3371@gmail.com>
2024-06-06 22:14:27 +0200

    test

--- src/php/loadImages.php
+++ src/php/loadImages.php
@@ -6,14 +6,13 @@ $imagePath = $docRoot . '/uploads';
 $images = scandir($imagePath);
 $files = array();
 
-
 foreach($images as $image) {
     $fullImage = $imagePath . $image;
     if (is_file($fullImage)) {
         // URL encode the file path for displaying in the HTML
         $encodedImage = str_replace('?', '%3F', $image);
         $fullImage = $imagePath . $encodedImage;
-        array_push($files, htmlspecialchars($image));
+        array_push($files, htmlspecialchars($image).' - test - '.$image);
     }
 }
 
