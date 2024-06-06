7e29f59d34073bdd82c677ec18f8fac7cad7c468
Gustavio <gust3371@gmail.com>
2024-06-06 20:23:14 +0200

    debug

--- src/js/logout.js
+++ src/js/logout.js
@@ -1,6 +1,9 @@
 jQuery(document).ready(function ($) {
+    console.log('ready');
     $('#topbar').on('load', function () {
+        console.log('load');
         $('#logoutBtn').click(function () {
+            console.log('click');
             $.ajax({
                 type: 'POST',
                 url: '/src/php/logout.php',
