d752a0e8bc7bfdecad84539bf972b5179451a8ed
Johan Uglebjerg Høck <hoeckjohanged@gmail.com>
2024-06-06 15:07:41 +0200

    fixed another typo

--- src/py/qrGen.py
+++ src/py/qrGen.py
@@ -5,8 +5,8 @@ import sys
 
 domain = sys.argv[1]
 url = pyqrcode.create(domain)
-url.png("/var/www/slideshow/src/picture/qrcode.png", scale=10)
-url.svg("/var/www/slideshow/src/picture/qrcode.svg", scale=10)
+url.png("/var/www/slideshow/src/pictures/qrcode.png", scale=10)
+url.svg("/var/www/slideshow/src/pictures/qrcode.svg", scale=10)
 
 
 
