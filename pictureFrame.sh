ff6ec14af43f31aad7c4d0683a303c205c757a1f
Gustavio <89874603+GustavZJ@users.noreply.github.com>
2024-06-06 09:48:34 +0200

    Update pictureFrame.sh

--- pictureFrame.sh
+++ pictureFrame.sh
@@ -30,4 +30,4 @@ if $autoremove; then
 fi
 
 # Run feh with the specified parameters as the user
-feh --auto-rotate -q -p -Z -F -R 60 -Y -D "$timedelay" /var/www/slideshow/uploads/*
+feh --auto-rotate -q -p -Z -F -R 60 -Y -D "$timedelay" /var/www/slideshow/uploads
