3378431a02a493795118e4a980e5584f2b5ada23
Gustavio <gust3371@gmail.com>
2024-06-07 13:56:49 +0200

    Stuff

--- src/js/loadImages.js
+++ src/js/loadImages.js
@@ -2,47 +2,52 @@ import { messageFade } from '/src/js/errorMessage.js';
 import { disableBtns } from '/src/js/deleteImage.js';
 
 jQuery(document).ready(function ($) {
-    function decodeHtmlEntities(target, str) {
-        return $(target).html(str).text();
+    function decodeHtmlEntities(str) {
+        var txt = document.createElement('textarea');
+        txt.innerHTML = str;
+        return txt.value;
     }
 
     $.ajax({
         type: 'POST',
         url: '/src/php/loadImages.php',
         success: function(response) {
-            if (response) {
-                $('#deleteBtn, #deleteAllBtn').css('display', 'inline-block');
-                $('#deletePreviewText').css('display', 'none');
+            if (response && Array.isArray(response)) {
+                if (response.length > 0) {
+                    $('#deleteBtn, #deleteAllBtn').css('display', 'inline-block');
+                    $('#deletePreviewText').css('display', 'none');
+                }
+
+                response.forEach(file => {
+                    const $imageCont = $('<div>', { class: 'imageCont elePointerIcon' });
+
+                    $imageCont.on('click', function() {
+                        const $checkbox = $(this).find('input[type="checkbox"]');
+                        $checkbox.prop('checked', !$checkbox.prop('checked'));
+                        disableBtns();
+
+                        if ($checkbox.prop('checked')) {
+                            $('#deleteBtn').removeAttr('disabled');
+                        }
+                    });
+
+                    const decodedFile = decodeHtmlEntities(file);
+                    const $img = $('<img>', { class: 'previewImage', src: `/uploads/${decodedFile}` });
+                    const $checkbox = $('<input>', {
+                        name: 'files[]',
+                        value: decodedFile,
+                        type: 'checkbox'
+                    });
+
+                    $checkbox.on('click', function(event) {
+                        event.stopPropagation();
+                        disableBtns();
+                    });
+
+                    $imageCont.append($img).append($checkbox);
+                    $('#imagePreviewCont').append($imageCont);
+                });
             }
-
-            response.forEach(file => {
-                const $imageCont = $('<div>', { class: 'imageCont elePointerIcon' });
-
-                $imageCont.on('click', function(event) {
-                    const $checkbox = $(this).find('input[type="checkbox"]');
-                    $checkbox.prop('checked', !$checkbox.prop('checked'));
-                    disableBtns();
-
-                    if ($checkbox.prop('checked')) {
-                        $('#deleteBtn').removeAttr('disabled');
-                    }
-                });
-
-                const $img = $('<img>', { class: 'previewImage', src: `/uploads/${file}` });
-                const $checkbox = $('<input>', {
-                    name: 'files[]',
-                    value: decodeHtmlEntities(this, file),
-                    type: 'checkbox'
-                });
-
-                $checkbox.on('click', function(event) {
-                    event.stopPropagation();
-                    disableBtns();
-                });
-
-                $imageCont.append($img).append($checkbox);
-                $('#imagePreviewCont').append($imageCont);
-            });
         },
         error: function() {
             messageFade('error', 'Noget gik galt med at loade billeder, prøv at genindlæse siden.');
